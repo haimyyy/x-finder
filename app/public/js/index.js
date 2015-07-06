0c2b3592200c1f0afd18e903752fd4331aa19d14
haimyyy <haimyyy@gmail.com>
2015-07-06 15:21:17 +0300

    design the screens

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,7 +1,7 @@
 /* global vars */
 g_domainDev="http://localhost:8080/";
 g_domainPro="http://x-find.herokuapp.com/";
-g_domain = g_domainPro;
+g_domain = g_domainDev;
 
 g_userInfo = null;
 g_loc = new GPSLocation();
@@ -54,7 +54,15 @@ function handleNavBarClick(obj){
 	}
 }
 */
-
+// function tog(v){return v?'addClass':'removeClass';} 
+// 	$(document).on('input', '.clearable', function(){
+// 	    $(this)[tog(this.value)]('x');
+// 	}).on('mousemove', '.x', function( e ){
+// 	    $(this)[tog(this.offsetWidth-18 < e.clientX-this.getBoundingClientRect().left)]('onX');   
+// 	}).on('touchstart click', '.onX', function( ev ){
+// 	    ev.preventDefault();
+// 	    $(this).removeClass('x onX').val('').change();
+// });
 
 $(window).resize(function() {
 	initPageCss();

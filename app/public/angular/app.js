53b0533b9bb1d2100ded6ab6f010e664291ea28c
avishay <avishayhajbi@gmail.com>
2015-07-18 23:47:42 +0300

    k

--- app/public/angular/app.js
+++ app/public/angular/app.js
@@ -4,7 +4,6 @@ var tempPermissions = ["email","public_profile","user_friends"];
 var userDetails = "?fields=id,name,picture{url},email,gender,first_name,last_name,relationship_status,significant_other,locale,work,hometown,events,tagged,location";
 var model = {
   user : {},
-  // domain: "http://localhost:8080/",
   domain: "http://x-find.herokuapp.com/",
   //domain:(document.domain == 'localhost')?'http://localhost:8080/':"http://x-find.herokuapp.com/",
   user_target:{

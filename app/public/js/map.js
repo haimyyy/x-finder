e7485db8dbe1ca8b378c0356eead35aefd1bd2e6
avishay <avishayhajbi@gmail.com>
2015-07-19 00:05:00 +0300

    prevent map scrolling while popup active

--- app/public/js/map.js
+++ app/public/js/map.js
@@ -73,6 +73,7 @@ var MapSingelton = (function (){
 	  	
 	  	google.maps.event.addListener(_map, 'click', function() {
 		   if (_info_window)_info_window.close();
+		    _map.setOptions( {draggable:true} ); 
 	    });
 	}
 	
@@ -115,6 +116,7 @@ var MapSingelton = (function (){
     });
 	if (_info_window)_info_window.close();
     _info_window=infoBubble;
+    _map.setOptions( {draggable:false} ); 
     _info_window.open();
 	}
 	
